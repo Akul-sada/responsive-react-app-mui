2089d163e085720581c462690beafb7bf5190c51
Akul-sada <sadananda.sherigara@gmail.com>
2023-03-24 06:33:35 +0000

    add responsive Navbar add side bar with icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,85 @@
-import styled from '@emotion/styled';
-import { Attractions } from '@mui/icons-material';
-import { AppBar, Toolbar, Typography, Box } from '@mui/material';
-import { theme } from '../theme';
-import React from 'react';
+import styled from "@emotion/styled";
+import { Attractions, Mail, Notifications } from "@mui/icons-material";
+import { AppBar, Toolbar, Typography, Box, InputBase, Badge, Avatar, Menu, MenuItem } from "@mui/material";
+import { theme } from "../theme";
+import React, { useState } from "react";
 
 const StyledToolbar = styled(Toolbar)({
-  display:"flex",
-  justifyContent:"space-between"
+  display: "flex",
+  justifyContent: "space-between",
 });
-const Search = styled("div")(({theme})=>({
-  backgroundColor:"white"
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  width: "40%",
 }));
-const Icons = styled(Box)(({theme})=>({
-  backgroundColor:"white"
-}))
+const Icons = styled(Box)(({ theme }) => ({
+  display:"none",
+  gap:"20px",
+  alignItems:"center",
+  [theme.breakpoints.up("sm")]:{
+    display:"flex"
+  }
+ 
+}));
+const UserBox = styled(Box)(({ theme }) => ({
+  display:"flex",
+  gap:"10px",
+  alignItems:"center",
+  [theme.breakpoints.up("sm")]:{
+    display:"none"
+  }
+ 
+ 
+}));
 
 const Navbar = () => {
+  const [open,setOpen] =useState(false);
   return (
-
-    <AppBar position='sticky'>
+    <AppBar position="sticky">
       <StyledToolbar>
-      <Typography variant='h6' sx={{display:{xs:"none",sm:"block"}}}>Akul Dev</Typography>
-      {/* Attractions is an icon */}
-      <Attractions sx={{display:{xs:"block", sm:"none"}}}/>
-      <Search>search</Search>
-      <Icons>icons</Icons>
-       
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Akul Dev
+        </Typography>
+        {/* Attractions is an icon */}
+        <Attractions sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..."></InputBase>
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail/>
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications/>
+          </Badge>
+          <Avatar sx={{width:30,height:30}} src="https://images.pexels.com/photos/11131486/pexels-photo-11131486.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" onClick={e=>setOpen(true)}/>
+        </Icons>
+          <UserBox>
+          <Avatar sx={{width:30,height:30}} src="https://images.pexels.com/photos/11131486/pexels-photo-11131486.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" onClick={e=>setOpen(true)}/>
+          <Typography variant="span">Rachel</Typography>
+          </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={e=>setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
-  
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,81 @@
-import { Box } from '@mui/material'
+import { Article, Home, ModeNight, People, Person, Portrait, Settings } from '@mui/icons-material';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
 import React from 'react'
 
 const Sidebar = () => {
   return (
-    <Box bgcolor={'skyblue'} flex={1} p={2} sx={{display:{xs:"none",sm:"block"}}}>Sidebar</Box>
-  )
+    <Box flex={1} p={2} sx={{display:{xs:"none",sm:"block"}}}>
+    <List>
+      <ListItem disablePadding>
+      <ListItemButton component="a" href="#home">
+      <ListItemIcon>
+        <Home/>
+      </ListItemIcon>
+      <ListItemText primary="Homepage"/>
+      </ListItemButton>
+      </ListItem>
+
+      <ListItem disablePadding>
+      <ListItemButton component="a" href="#home">
+      <ListItemIcon>
+        <Article/>
+      </ListItemIcon>
+      <ListItemText primary="Pages"/>
+      </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+
+      <ListItemButton component="a" href="#home">
+      <ListItemIcon>
+        <People/>
+      </ListItemIcon>
+      <ListItemText primary="Groups"/>
+      </ListItemButton>
+      </ListItem>
+
+      <ListItem disablePadding>
+      <ListItemButton component="a" href="#home">
+      <ListItemIcon>
+        <People/>
+      </ListItemIcon>
+      <ListItemText primary="MarketPlace"/>
+      </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+      <ListItemButton component="a" href="#home">
+      <ListItemIcon>
+        <Person/>
+      </ListItemIcon>
+      <ListItemText primary="Friends"/>
+      </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+      <ListItemButton component="a" href="#home">
+      <ListItemIcon>
+        <Settings/>
+      </ListItemIcon>
+      <ListItemText primary="Settings"/>
+      </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+      <ListItemButton component="a" href="#home">
+      <ListItemIcon>
+        <Portrait/>
+      </ListItemIcon>
+      <ListItemText primary="Profile"/>
+      </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+      <ListItemButton component="a" href="#home">
+      <ListItemIcon>
+        <ModeNight/>
+      </ListItemIcon>
+      <Switch/>
+      </ListItemButton>
+      </ListItem>
+    </List>
+    </Box>
+  );
 }
 
 export default Sidebar
